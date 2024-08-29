1110051ba6cce75074562fedc0e648307965c7a3
AMIR SHAHZAD HASHMI <amirshahzadhashmi7145@gmail.com>
2024-08-29 23:27:50 +0500

    Containerize the Application

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // This allows Vite to be accessed externally
+    port: 5173,       // Optional: specify the port, default is 5173
+  }
 })
